105bdc3c627ae8a410383208efb55d480abaaa04
FrostFoe <frostfoe@gmail.com>
2025-11-07 10:04:39 +0000

    add this as partners
    ```
    
    <div class="container pb-2rem bg-black-rich-f

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import HeroSkills from "@/components/landing/hero-skills";
 import HeroStats from "@/components/landing/hero-stats";
 import HeroWork from "@/components/landing/hero-work";
 import Nav from "@/components/nav";
+import Partners from "@/components/landing/partners";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
         <HeroDomains />
         <HeroWork />
         <HeroCarrer />
+        <Partners />
         <Footer />
       </main>
     </div>
