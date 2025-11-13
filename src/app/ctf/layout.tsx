5f49f1877353a9c19f265a7bf2fe77106a3b0886
FrostFoe <frostfoe@gmail.com>
2025-11-13 07:31:04 +0000

    push

--- src/app/ctf/layout.tsx
+++ src/app/ctf/layout.tsx
@@ -5,7 +5,7 @@ export default function CTFLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex">
       <CTFSidebar />
-      <main className="flex-1 md:ml-0">
+      <main className="flex-1 md:ml-64">
         {children}
       </main>
     </div>
